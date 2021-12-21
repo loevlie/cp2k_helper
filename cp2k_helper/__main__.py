a186087a2327b991d1549dd5a0f025a0e73e9a31
loevliedenny@gmail.com <59749099+loevlie@users.noreply.github.com>
2021-12-21 17:07:06 -0500

    Fixing a printing issue

--- cp2k_helper/__main__.py
+++ cp2k_helper/__main__.py
@@ -2,13 +2,15 @@ import sys
 from cp2k_helper.cp2k_helper import output_parser,Summ
 
 def main():
-    print('CREATING RESTART')
+    
     args = sys.argv[1:]
 
     if args[0]=='--restart':
+        print('CREATING RESTART')
         parser_ = output_parser(base_file_path='.',depth=1)
         parser_.restart_job()
     if args[0] =="--summ":
+        print(f"CREATING SUMMARY")
         Summ(args[1])
 
 
